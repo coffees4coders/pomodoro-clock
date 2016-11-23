24a3fe86a737223acd54f796581eadf56f3f7bd6
Jay Ullman <jayullman@gmail.com>
2016-11-22 19:11:06 -0800

    Change 'session' to 'focus'

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /** NOTES
 
-TODO: change session to focus
+TODO: change focus to focus
 TODO: give reset button functionality
 
 NOTE: Break timer object into two timers, Focus and Break
@@ -16,8 +16,8 @@ document.addEventListener('DOMContentLoaded', function() {
   // Add event listeners for all UI buttons
   var breakMinusButton = document.getElementById('break-minus'),
       breakPlusButton = document.getElementById('break-plus'),
-      sessionMinusButton = document.getElementById('session-minus'),
-      sessionPlusButton = document.getElementById('session-plus'),
+      focusMinusButton = document.getElementById('focus-minus'),
+      focusPlusButton = document.getElementById('focus-plus'),
       startButton = document.getElementById('start-button'),
       stopButton = document.getElementById('stop-button'),
       resetButton = document.getElementById('reset-button');
@@ -47,19 +47,19 @@ document.addEventListener('DOMContentLoaded', function() {
 
   });
 
-  sessionMinusButton.addEventListener('click', function() {
+  focusMinusButton.addEventListener('click', function() {
     if (timer.timerRunning === false) {
-      var sessionTimer = document.getElementById('session-time'),
-          mainCountdownTimer = document.getElementById('main-countdown-timer');
+      var focusTimer = document.getElementById('focus-time'),
+          mainCountdownTimer = document.getElementById('focus-timer');
 
-      if (timer.sessionSetting > 60) {
-        timer.sessionSetting -= 60;
+      if (timer.focusSetting > 60) {
+        timer.focusSetting -= 60;
         // rounds down to the nearest minute
-        timer.sessionSetting = parseInt(timer.sessionSetting / 60) * 60;
-        updateDisplay(sessionTimer, timer.sessionSetting);
-        updateDisplay(mainCountdownTimer, timer.sessionSetting, true);
+        timer.focusSetting = parseInt(timer.focusSetting / 60) * 60;
+        updateDisplay(focusTimer, timer.focusSetting);
+        updateDisplay(mainCountdownTimer, timer.focusSetting, true);
 
-        // ensures that after resetting session length,
+        // ensures that after resetting focus length,
         // timer will restart at new lenght
         timer.resetOnStart = true;
 
@@ -67,19 +67,19 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   });
 
-  sessionPlusButton.addEventListener('click', function() {
+  focusPlusButton.addEventListener('click', function() {
     if (timer.timerRunning === false) {
-      var sessionTimer = document.getElementById('session-time'),
-          mainCountdownTimer = document.getElementById('main-countdown-timer'),
+      var focusTimer = document.getElementById('focus-time'),
+          mainCountdownTimer = document.getElementById('focus-timer'),
           minutesRoundedDown;
 
       // add 60 seconds and round down to the nearest minute
-      timer.sessionSetting += 60;
-      timer.sessionSetting = parseInt(timer.sessionSetting / 60) * 60;
-      updateDisplay(sessionTimer, timer.sessionSetting);
-      updateDisplay(mainCountdownTimer, timer.sessionSetting, true);
+      timer.focusSetting += 60;
+      timer.focusSetting = parseInt(timer.focusSetting / 60) * 60;
+      updateDisplay(focusTimer, timer.focusSetting);
+      updateDisplay(mainCountdownTimer, timer.focusSetting, true);
 
-      // ensures that after resetting session length,
+      // ensures that after resetting focus length,
       // timer will restart at new lenght
       timer.resetOnStart = true;
 
@@ -153,27 +153,27 @@ function removeClass(elem, style) {
 }
 
 /**
-   This function is called whenever a session or break session ends
-   Takes string as parameter: session or break, depending on which is ending
+   This function is called whenever a focus or break focus ends
+   Takes string as parameter: focus or break, depending on which is ending
 */
-function timerFinished(breakOrSession) {
+function timerFinished(breakOrfocus) {
   var alertDiv = document.getElementById('alert-box'),
       alertText = document.getElementById('alert-text'),
       alertButton = document.getElementById('alert-button'),
-      alertButtonText = document.getElementById('break-or-session-text');
+      alertButtonText = document.getElementById('break-or-focus-text');
 
   timer.resetOnStart = true;
 
   addClass(alertDiv, 'show');
 
-  if (breakOrSession === 'break') {
+  if (breakOrfocus === 'break') {
     alertButton.addEventListener('click', function() {
       timer.startTimer('focus');
       removeClass(alertDiv, 'show');
     });
 
 
-    alertButtonText.innerHTML = 'Focus Session';
+    alertButtonText.innerHTML = 'Focus focus';
   } else {
     alertButton.addEventListener('click', function() {
       timer.startTimer('break');
@@ -187,7 +187,7 @@ function timerFinished(breakOrSession) {
 function alertButtonClick() {
   // takes the string from the alert button, which would be 'break' or 'focus'
   // depending on what part of the cycle it is in
-  var focusOrBreak = document.getElementById('break-or-session-text').toLowerCase();
+  var focusOrBreak = document.getElementById('break-or-focus-text').toLowerCase();
 
   if (focusOrBreak === 'break') {
 
@@ -237,10 +237,10 @@ function convertSecondsToDisplayTime(seconds, includeSeconds) {
 }
 
 
-// break this up into two objects to handle break and session timers, maybe
+// break this up into two objects to handle break and focus timers, maybe
 var timer = {
   // all units of time are in seconds
-  sessionSetting: 1500,
+  focusSetting: 1500,
   breakSetting: 300,
 
   currentSeconds: 1500,
@@ -251,12 +251,12 @@ var timer = {
   // other actions from taking place
   timerRunning: false,
 
-  // determines whether the countdown resets at the break or session length
+  // determines whether the countdown resets at the break or focus length
   // setting when pressing start.
   resetOnStart: false,
 
   // takes two parameters
-  // timer specifies if it's the break or session timer
+  // timer specifies if it's the break or focus timer
   // seconds is the number of seconds
   updateTimer: function(timer, seconds) {
     // countDownDisplay.innerHTML = convertSecondsToDisplayTime(seconds);
@@ -266,14 +266,14 @@ var timer = {
   // focus or break setting. Takes a string: 'break' or 'focus'
   startTimer: function(focusOrBreak) {
     // main countdown clock
-    var mainCountDownDisplay = document.getElementById('main-countdown-timer');
+    var mainCountDownDisplay = document.getElementById('focus-timer');
 
     this.timerRunning = true;
 
 
     if (timer.resetOnStart) {
       if (focusOrBreak === 'focus') {
-        this.currentSeconds = this.sessionSetting;
+        this.currentSeconds = this.focusSetting;
     } else {
         this.currentSeconds = this.breakSetting;
       }
@@ -298,7 +298,7 @@ var timer = {
 
     if (timer.currentSeconds === 0) {
       timer.stopTimer();
-      timerFinished('session');
+      timerFinished('focus');
     }
 
   }, 250);
